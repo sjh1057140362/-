03992373f2991eeb817d12fedb2b12fdc6bf834f
sjh1057140362@outlook.com <sjh1057140362@outlook.com>
2024-05-06 20:48:18 +0800

    详情基础渲染

--- src/router/index.js
+++ src/router/index.js
@@ -4,6 +4,7 @@ import Layout from "@/views/Layout/index.vue";
 import Home from "@/views/Home/index.vue";
 import Category from "@/views/Category/index.vue";
 import SubCategory from '@/views/SubCategory/index.vue'
+import Detail from '@/views/Detail/index.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -19,10 +20,15 @@ const router = createRouter({
         },
         { path: "category/:id", component: Category },
         { path: "category/sub/:id", component: SubCategory },
+        { path: "detail/:id", component: Detail },
       ],
     },
     { path: "/login", component: Login },
   ],
+  // 路由定制
+  scrollBehavior(){
+    return {top:0}
+  }
 });
 
 export default router;
