e8699a898c43cff6752587a2fbb0771450bf2f20
sjh1057140362@outlook.com <sjh1057140362@outlook.com>
2024-05-13 20:58:37 +0800

    会员中心的路由配置

--- src/router/index.js
+++ src/router/index.js
@@ -6,9 +6,12 @@ import Category from "@/views/Category/index.vue";
 import SubCategory from "@/views/SubCategory/index.vue";
 import Detail from "@/views/Detail/index.vue";
 import CartList from "@/views/CartList/index.vue";
-import Checkout from "@/views/Checkout/index.vue"
-import Pay from '@/views/Pay/index.vue'
-import PayBack from '@/views/Pay/PayBack.vue'
+import Checkout from "@/views/Checkout/index.vue";
+import Pay from "@/views/Pay/index.vue";
+import PayBack from "@/views/Pay/PayBack.vue";
+import Member from "@/views/Member/index.vue";
+import UserInfo from "@/views/Member/components/UserInfo.vue";
+import UserOrder from "@/views/Member/components/UserOrder.vue";
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -29,6 +32,14 @@ const router = createRouter({
         { path: "checkout", component: Checkout },
         { path: "pay", component: Pay },
         { path: "paycallback", component: PayBack },
+        {
+          path: "member",
+          component: Member,
+          children: [
+            { path: "user", component: UserInfo },
+            { path: "order", component: UserOrder },
+          ],
+        },
       ],
     },
     { path: "/login", component: Login },
